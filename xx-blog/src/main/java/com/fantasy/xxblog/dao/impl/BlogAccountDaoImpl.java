e6b7bb74665b0c4406fbd342e4ad47a252f26751
lifang <lifang@winployee.com>
2017-09-12 20:56:01 +0800

    多数据源功能完善提交

--- xx-blog/src/main/java/com/fantasy/xxblog/dao/impl/BlogAccountDaoImpl.java
+++ xx-blog/src/main/java/com/fantasy/xxblog/dao/impl/BlogAccountDaoImpl.java
@@ -1,7 +1,6 @@
 package com.fantasy.xxblog.dao.impl;
 
 import com.fantasy.xxbase.dao.BaseDaoImpl;
-import com.fantasy.xxbase.datasource.DataSource;
 import com.fantasy.xxblog.dao.BlogAccountDao;
 import com.fantasy.xxblog.entity.BlogAccountEntity;
 import org.springframework.stereotype.Repository;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Repository;
  * @sine 17/03/13
  */
 @Repository
-@DataSource
 public class BlogAccountDaoImpl extends BaseDaoImpl<BlogAccountEntity> implements BlogAccountDao {
 
 
